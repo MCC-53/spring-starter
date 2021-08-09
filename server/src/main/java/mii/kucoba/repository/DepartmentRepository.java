c4f05a2e7cd7053e930604754c5a2555cfc9d9d6
Aditya Tri Nugraha <58800581+Aditya965@users.noreply.github.com>
2021-08-09 18:30:42 +0700

    Tugas 1

---
+++ server/src/main/java/mii/kucoba/repository/DepartmentRepository.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mii.kucoba.repository;
+
+import java.util.List;
+import mii.kucoba.models.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author haikal
+ */
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Integer>{
+    Department findByEmployees_id(Integer id);
+    
+    @Query(value = "SELECT * FROM department WHERE status = 'true'", nativeQuery = true) //nativeQuery = true berarti menandakan bahwa query ini kita buat sendiri
+    List<Department> getTrueDepartment();
+}
