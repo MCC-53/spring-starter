14b2c963cc1e65e98910c16cd0dc2689552d87cd
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-08-09 11:25:13 +0700

    inserting cors implementation

---
+++ serverside/src/main/java/com/nieceoftimes/serverside/auth/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.nieceoftimes.serverside.auth.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    private final String[] DOMAINS = {"http://localhost:8087"};
+    private final String[] HTTPMETHODS = {"GET", "POST", "PUT", "DELETE"};
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(DOMAINS)
+                .allowedMethods(HTTPMETHODS)
+                .allowCredentials(true);
+
+    }
+}
