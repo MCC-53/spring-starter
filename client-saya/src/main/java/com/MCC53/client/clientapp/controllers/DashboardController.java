1543dc06ca3b24600c8cc204eefcdb3ed4b87a05
jodhipradana <75001003+jodhipradana@users.noreply.github.com>
2021-08-17 07:00:09 +0700

    Tugas 2

---
+++ client-saya/src/main/java/com/MCC53/client/clientapp/controllers/DashboardController.java
@@ -0,0 +1,21 @@
+package com.MCC53.client.clientapp.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+//@PreAuthorize("hasAnyRole('ADMIN','EMPLOYEE')")
+public class DashboardController {
+
+    @GetMapping
+//    @PreAuthorize("hasAuthority ('READ_DATA')")
+    public String index() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return "/index";
+    }
+}
